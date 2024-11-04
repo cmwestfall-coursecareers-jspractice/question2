991b9144aa4d8f511a2ea8742d53d2a89325bce7
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-11-04 14:10:27 -0500

    final commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-function stringLengthComparer(stringArr, intArr) {
+function stringLengthMatcher(stringArr, intArr) {
   let count = 0;
 
   if (intArr.length === 0) {
-    console.log(0);
+    console.log(count);
     return;
   }
 
   for (let string of stringArr) {
-    if (intArr.includes(string.length)) {
-      count++;
+    stringLength = string.length;
+    for (int of intArr) {
+      if (stringLength === int) {
+        count++;
+      }
     }
   }
   console.log(count);
 }
 
-stringLengthComparer(["this", "is", "a", "test"], [4, 2]);
+stringLengthMatcher(["this", "is", "a", "test"], [4, 2]);
